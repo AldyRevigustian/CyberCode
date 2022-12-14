14d4be4cd3b2cc31849f19ed8d00a7510975c26c
Dey <72060143+AldyRevigustian@users.noreply.github.com>
2022-12-14 21:26:43 +0700

    gajadi smooth :(

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -256,28 +256,6 @@
         })
     </script>
 
-    <script>
-        const body = document.body,
-            scrollWrap = document.getElementsByClassName("smooth-scroll-wrapper")[0],
-            height = scrollWrap.getBoundingClientRect().height - 1,
-            speed = 0.04;
-
-        var offset = 0;
-
-        body.style.height = Math.floor(height) + "px";
-
-        function smoothScroll() {
-            offset += (window.pageYOffset - offset) * speed;
-
-            var scroll = "translateY(-" + offset + "px) translateZ(0)";
-            scrollWrap.style.transform = scroll;
-
-            callScroll = requestAnimationFrame(smoothScroll);
-        }
-
-        smoothScroll();
-    </script>
-
     @livewireScripts
 
 </body>
